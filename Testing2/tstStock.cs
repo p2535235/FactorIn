f2bcbc929b763380971c39a63819ccf15abbead2
Grizell Dsouza <P2538413@my365.dmu.ac.uk>
2021-02-12 21:18:38 +0000

    Added testing and class for stock management

---
+++ Testing2/tstStock.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Testing2
+{
+    [TestClass]
+    public class tstStock
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            //create an instance of a class we want to create 
+            clsStock aStock = new clsStock();
+
+            //test to see that exists 
+            Assert.IsNotNull(aStock);
+
+        }
+    }
+}
