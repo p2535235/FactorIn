a71648f0114a230a4d7522d6784ad6ff40c83008
Oliver Davis <P2535235@my365.dmu.ac.uk>
2021-02-12 15:22:24 +0000

    Created the methods and tests for the Staff management properties

--- ClassLibrary/ClsStaff.cs
+++ ClassLibrary/ClsStaff.cs
@@ -2,5 +2,12 @@
 {
     public class clsStaff
     {
+        public bool Active { get; set; }
+        public int StaffID { get; set; }
+        public string StaffName { get; set; }
+        public System.DateTime DateAccessed { get; set; }
+        public double TotalCost { get; set; }
+        public bool GrantAccess { get; set; }
+        public int ProductID { get; set; }
     }
 }
