4bfed3a5684f17b2f6a0b1c58d76db7972558361
Grizell Dsouza <P2538413@my365.dmu.ac.uk>
2021-02-12 23:00:17 +0000

    Added properties and respective tests to stock class

--- ClassLibrary/clsStock.cs
+++ ClassLibrary/clsStock.cs
@@ -1,6 +1,14 @@
-﻿namespace ClassLibrary
+﻿using System;
+
+namespace ClassLibrary
 {
     public class clsStock
     {
+        public bool Availability { get; set; }
+        public DateTime DateUpdated { get; set; }
+        public int ProductID { get; set; }
+        public int Quantity { get; set; }
+        public string Location { get; set; }
+        public double Price { get; set; }
     }
 }
