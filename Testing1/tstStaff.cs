a5bf125937272009af3c269c5d341b6611b14b8a
Oliver Davis <P2535235@my365.dmu.ac.uk>
2021-02-12 14:45:41 +0000

    Added testing and class for staff management.

---
+++ Testing1/tstStaff.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Testing1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void instanceOK()
+        {
+            // Create an instance of the class we want to create.
+            clsStaff staff = new clsStaff();
+            // Test to see that it exists.
+            Assert.IsNotNull(staff);
+        }
+    }
+}
